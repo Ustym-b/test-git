0367a69848517ad82d08d9c38ab183c1cc18d10d
Ustym <ystim2314@gmail.com>
2023-04-22 13:37:05 +0300

    My first commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// alert('hello');
+
+// const result = confirm('A . [th&');
+// console.log(result);
+
+// const answer = +prompt('ваМ Є 18', '18');
+// console.log(typeof(answer) );
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?','');
+// answers[1] = prompt('What is your searname?','');
+// answers[2] = prompt('Age?','');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const user = 'Ustym';
+
+// console.log(`Доброго вечора ${user}`);
+// let incr = 10, decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(incr);
+// console.log(decr);
+
+const isChec = false,
+	isClose = false;
+
+console.log(isChec && isClose);
