aa2cbe8f9a1390579bc8caa3da0e7ee0e867140f
Ustym <ystim2314@gmail.com>
2023-04-22 14:40:45 +0300

    початок роботи над своїм першим додатком

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?','');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+let movieFirst = prompt('Один з останіх переглянутих фільмів?',''),
+	movieFirstBal = prompt('Яку оцінку йому двсте?',''),
+	movieTwo = prompt('Один з останіх переглянутих фільмів?',''),
+	movieTwoBal = prompt('Яку оцінку йому двсте?','');
+
+personalMovieDB.movies[movieFirst] = movieFirstBal;
+personalMovieDB.movies[movieTwo] = movieTwoBal;
+
+console.log(personalMovieDB);
 // alert('hello');
 
 // const result = confirm('A . [th&');
@@ -28,7 +47,7 @@
 // console.log(incr);
 // console.log(decr);
 
-const isChec = false,
-	isClose = false;
+// const isChec = false,
+// 	isClose = false;
 
-console.log(isChec && isClose);
+// console.log(isChec && isClose);
