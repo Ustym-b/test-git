ff6eb76156e7a8d2b3c457ac66cbbed95fdb0262
Ustym <ystim2314@gmail.com>
2023-04-24 21:43:58 +0300

    Home work

--- js/index.js
+++ js/index.js
@@ -19,35 +19,3 @@ personalMovieDB.movies[movieFirst] = movieFirstBal;
 personalMovieDB.movies[movieTwo] = movieTwoBal;
 
 console.log(personalMovieDB);
-// alert('hello');
-
-// const result = confirm('A . [th&');
-// console.log(result);
-
-// const answer = +prompt('ваМ Є 18', '18');
-// console.log(typeof(answer) );
-
-// const answers = [];
-
-// answers[0] = prompt('What is your name?','');
-// answers[1] = prompt('What is your searname?','');
-// answers[2] = prompt('Age?','');
-
-// console.log(typeof(answers));
-// console.log(typeof(null));
-
-// const user = 'Ustym';
-
-// console.log(`Доброго вечора ${user}`);
-// let incr = 10, decr = 10;
-
-// ++incr;
-// --decr;
-
-// console.log(incr);
-// console.log(decr);
-
-// const isChec = false,
-// 	isClose = false;
-
-// console.log(isChec && isClose);
