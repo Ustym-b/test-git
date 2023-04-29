f344880635dc4f43d3d28db9cff0b03e62d3d778
Ustym <ystim2314@gmail.com>
2023-04-29 22:47:44 +0300

    Home work add function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 'use strict';
 
-const numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?','');
+let numberOfFilms;
+
+function start(){
+	numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?','');
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+		numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?','');
+	}
+}
+
+start();
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -10,12 +20,89 @@ const personalMovieDB = {
 	privat: false
 };
 
-let movieFirst = prompt('Один з останіх переглянутих фільмів?',''),
-	movieFirstBal = prompt('Яку оцінку йому двсте?',''),
-	movieTwo = prompt('Один з останіх переглянутих фільмів?',''),
-	movieTwoBal = prompt('Яку оцінку йому двсте?','');
+function rememberMyFilms(){
+	for(let i = 0; i < 2; i++){
+		let a = prompt('Один з останіх переглянутих фільмів?',''),
+			b = prompt('Яку оцінку йому дасте?','');
+		if(a === '' || b === '' || a === null || b === null || a.length >= 50 ||b.length >= 50){
+			console.log('error');
+			i--;
+			
+		} else {
+			console.log('done');
+			personalMovieDB.movies[a] = b;
+		}
+	}
+}
 
-personalMovieDB.movies[movieFirst] = movieFirstBal;
-personalMovieDB.movies[movieTwo] = movieTwoBal;
+// rememberMyFilms();
 
-console.log(personalMovieDB);
+function detectPersonalLevel(){
+	if(personalMovieDB.count < 10){
+		alert('Переглянуто доволі мало фільмів');
+	} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+		alert('Ви класичний глядач');
+	} else if(personalMovieDB.count >= 30){
+		alert('Ви кіноман');
+	} else{
+		alert('Сталася помилка');
+	}
+}
+
+detectPersonalLevel();
+
+function writeYourGenres(){
+	for(let i = 0; i < 3; i++){
+		personalMovieDB.genres[i] = prompt(`Який ваш улюблений жанр під номером ${i+1}?`);
+	}
+}
+
+writeYourGenres();
+
+function showMyDB(hidden){
+	if (!hidden){
+		console.log(personalMovieDB);
+	}
+}
+
+showMyDB(personalMovieDB.privat);
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++){
+// 	for(let j = 0; j < lines - i; j++){
+// 		result+= ' ';
+// 	}
+// 	for(let j = 0; j < 2*i+1; j++){
+// 		result+= '*';
+// 	}
+// 	result += '\n';
+// }
+// console.log(result);
+
+
+// function getMathResult(basicNum, basicProgres) {
+// 	let str = '';
+
+// 	if(typeof(basicProgres) === 'number' && basicProgres > 0){
+
+
+// 		for(let i = 1; i<= basicProgres; i++){
+// 			if(i == basicProgres){
+// 				str+= `${basicNum * i}`;
+// 			} else {
+// 				str+= `${basicNum * i} --- `;
+// 			}
+
+// 		}
+		
+// 		console.log('Done');
+// 		return str;
+// 	} else {
+// 		return basicNum;
+// 	}
+// }
+
+// console.log(getMathResult(5, 2)); 
+
